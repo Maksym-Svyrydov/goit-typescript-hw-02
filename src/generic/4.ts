74db64f9cc025d34f677032d71292393ef74af81
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-12-19 23:34:28 +0200

    baseTypes+generic

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,17 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface ComponentProps {}
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+interface PageProps extends ComponentProps {
+  title: string;
+}
+class Page extends Component<PageProps> {
+  pageInfo() {
+    console.log(this.props.title);
+  }
+}
 
-// class Component {
-//   constructor (public props:T) {
-
-//   }
-// }
-
-// class Page extends Component {
-//   pageInfo () {
-//     console.log(this.props.title);
-//   }
-// }
-
-// export {};
+export {};
